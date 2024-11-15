58c59a14e56063c6a480fda590964da5844710c7
Prajayshee Chauhan <83404385+prajayshee@users.noreply.github.com>
2024-11-15 17:27:27 +0530

    Update urls.py

--- project1/urls.py
+++ project1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.task_list, name='task_list'),
+    path('', views.login_view, name='login'), 
     path('add/', views.add_task, name='add_task'),
     path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
